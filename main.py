5cacb7560ac4bc43cc41b393ef5f10e7d3ddb0e3
s520y <sean520yang@gmail.com>
2026-07-06 03:08:30 +0000

    beginning to add quote and scramble implementation

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import random
 
+
+
+
+def sattolo_cycle(items) -> None:
+    """Sattolo's algorithm."""
+    i = len(items)
+    while i > 1:
+        i = i - 1
+        j = random.randrange(i)  # 0 <= j <= i-1
+        items[j], items[i] = items[i], items[j]
 
 c = st.container()
 c.write("HI")
@@ -14,3 +25,14 @@ df = pd.DataFrame({
 })
 
 df
+
+alphabet = list("abcdefghijklmnopqrstuvwxyz")
+quotes = ["Whether you think you can or you think you cannot, you are correct."]
+
+selected_quote = random.choice(quotes)
+sattolo_cycle(alphabet)
+
+for c in selected_quote:
+    pass 
+
+print(alphabet)
