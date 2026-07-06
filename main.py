4808cd7b3dde7bb269cf3537c4b9185c3be37f6a
s520y <sean520yang@gmail.com>
2026-07-06 02:05:06 +0000

    added proper requirements

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 
+c = st.container()
+c.write("HI")
 st.title("Cryptograms :)")
 st.markdown("test")
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
